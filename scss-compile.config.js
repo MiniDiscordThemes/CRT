6bcd4e529f9564658d23de576c370550031f33c2
Saltssaumure <saltssaumure@gmail.com>
2023-04-30 19:59:59 +0100

    Create theme

--- scss-compile.config.js
+++ scss-compile.config.js
@@ -2,11 +2,11 @@
 export default {
     meta: {
         /** The official name of your theme. */
-        name: "Template",
+        name: "CRT",
         /** The name of your base scss file. */
-        scss: "temp",
+        scss: "crt",
         /** The name of your theme repository. */
-        repo: "template-discord-theme",
+        repo: "CRT",
         /** The version of your theme. */
         version: "1.0.0"
     }
