628ceda88edcf575e8dc58efd8856e67ab64c712
Saltssaumure <Saltssaumure@users.noreply.github.com>
2023-11-01 05:11:42 +0000

    deploy: b56c65e2e4cfaf3290900165a43104cb788fe48e

--- scss-compile.config.js
+++ scss-compile.config.js
@@ -1,13 +1,9 @@
 /** @type {import('bd-scss/lib/config').Config} */
 export default {
     meta: {
-        /** The official name of your theme. */
-        name: "CRT",
-        /** The name of your base scss file. */
-        scss: "crt",
-        /** The name of your theme repository. */
-        repo: "CRT",
-        /** The version of your theme. */
-        version: "1.0.3"
+        name: "CRT" /* The official name of your theme. */,
+        scss: "crt" /* The name of your base scss file. */,
+        repo: "CRT" /* The name of your theme repository. */,
+        version: "dev" /* The version of your theme. */
     }
 };
